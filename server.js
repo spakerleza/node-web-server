5c5176e34b68493860278f4fd21e8ab4364e0a08
Sab Udeh Chukwumdimma <chukwumdimma4life@gmail.com>
2017-10-09 20:54:48 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+// Middleware
+app.use((req, res, next) => {
+    var now = new Date().toDateString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile("server.log", log + "\n", (err) => {
+        if (err) {
+            console.log("Unable to append to server.log.");
+        }
+    });
+    next();
+});
+app.use((req, res, next) => {
+    res.render("maintenance");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("currentYear", () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", (text) => {
+    return text.toUpperCase();
+});
+
+
+app.get("/", (req, res) => {
+    res.render("home", {
+        pageTitle: "Home Page",
+        welcomeMessage: "Welcome to my first node web application.",
+        // currentYear: new Date().getFullYear(),
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", {
+        pageTitle: "About Page",
+        // currentYear: new Date().getFullYear(),
+    });
+});
+
+
+app.get("/bad", (req, res) => {
+    res.send({
+        error: "Bad request sent"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("server is up on port 3000");
+});
